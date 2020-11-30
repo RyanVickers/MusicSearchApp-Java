83a80676b24e3957988f5f5eba24fb8f7f674067
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-11-30 17:17:59 -0500

    -Added Basic GUI

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    /**
+     * Start application method
+     */
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Views/searchView.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/style.css");
+        stage.setScene(scene);
+        stage.setTitle("Spotify Search");
+        stage.show();
+    }
 }
