7f182e0a6779fb76d18c4b7eef016d94e95b3111
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-12-01 21:46:33 -0500

    -Added back buttons

--- src/Controllers/AlbumViewController.java
+++ src/Controllers/AlbumViewController.java
@@ -84,4 +84,5 @@ public class AlbumViewController implements Initializable {
             e.printStackTrace();
         }
     }
+
 }
--- src/Controllers/TrackViewController.java
+++ src/Controllers/TrackViewController.java
@@ -2,10 +2,14 @@ package Controllers;
 
 
 import Models.TrackData;
+import Models.TrackResult;
 import Utilities.JSONUtility;
 import Utilities.MusicApiUtility;
+import Utilities.SceneChangerUtility;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.io.IOException;
@@ -16,12 +20,38 @@ public class TrackViewController implements Initializable {
     @FXML
     private ListView<TrackData> trackListView;
 
+    @FXML
+    private Label rowsReturnedLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         trackListView.getItems().clear();
         try {
              trackListView.getItems().addAll(JSONUtility.getTrackList("src/JSONData/trackInfo.json"));
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        updateLabels();
+    }
+
+    private void updateLabels() {
+        rowsReturnedLabel.setText("Rows Returned: " + trackListView.getItems().size());
+    }
+
+    @FXML
+    private void backToDetailsView(ActionEvent event) {
+        try {
+            SceneChangerUtility.changeScene(event, "/Views/albumView.fxml", "Album Details");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void backToAlbumList(ActionEvent event) {
+        try {
+            SceneChangerUtility.changeScene(event, "/Views/searchView.fxml", "Album Search");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
